2cf2fc4faec4a62d47633a13751c990f506be0dc
Ataiskih <Ataiskih@users.noreply.github.com>
2020-06-06 16:49:11 +0600

    added authors and users

--- itblog/article/models.py
+++ itblog/article/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User         # добавление пользователей
+
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     likes = models.IntegerField(default=0)
     active = models.BooleanField(default=True)
+    author = models.ForeignKey(
+        to = "Author", on_delete=models.CASCADE,
+        related_name = "articles", null=True, blank=True
+    )
+    
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.OneToOneField(
+        to = User, on_delete = models.SET_NULL, 
+        related_name = "author", null = True, blank = True)
+    def __str__(self):
+        return self.name
--- itblog/article/views.py
+++ itblog/article/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Author
 
 
 def homepage(request):
     articles = Article.objects.all()
-
-    return render(request, "article/homepage.html", {"articles": articles})
+    lst_authour = Author.objects.get(id=1)
+    return render(request, "article/homepage.html",
+        {
+            "articles": articles, 
+            "lst_authour":lst_authour
+        })
